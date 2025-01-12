7c84acead4c2bce99451c1cbd20d07a01c273931
Bhavin <opbhavin21@gmail.com>
2025-01-12 17:48:49 +0530

    update the database

--- anime_site.sql
+++ anime_site.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 12, 2025 at 04:58 AM
+-- Generation Time: Jan 12, 2025 at 01:17 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -64,13 +64,21 @@ CREATE TABLE `episodes` (
 --
 
 INSERT INTO `episodes` (`episode_id`, `anime_id`, `episode_title`, `episode_url`) VALUES
-(1, 1, 'The Begining', '1o-6-aR4eGyxbmF2PJ3XT5M9EV1J_STm0'),
-(2, 2, 'Kimi No Namayva', '1yeirUYkEhVPXSWq-xhA7hlhCxqzWLbM3'),
-(3, 1, 'Second', '1yet2FV8rDV5HZkW7c85K0rwT0jWHt2Eu'),
-(4, 1, 'Third', '1bf4sF5X2xdxgefbRkxCuEoOpVdbE5mft'),
-(5, 1, 'Fourth', '1rU2kuj0NdPMtlXHoNit903Hw5Nbiv3J_'),
-(6, 3, 'The Begining of Gang', '1rTmSckwKJgPN6gzkTnxaONXjyewYImAT'),
-(7, 3, 'Second Finale', '1LM9RJeEm4CIgsuM59GA3KpB8SCHJgsZK');
+(1, 1, 'That\'s How Love Starts, Ya Know!', '1o-6-aR4eGyxbmF2PJ3XT5M9EV1J_STm0'),
+(2, 2, 'What is your Name ?', '1yeirUYkEhVPXSWq-xhA7hlhCxqzWLbM3'),
+(3, 1, 'That\'s a Space Alien, Ain\'t It?!', '1yet2FV8rDV5HZkW7c85K0rwT0jWHt2Eu'),
+(4, 1, 'It\'s a Granny vs Granny Clash!', '1bf4sF5X2xdxgefbRkxCuEoOpVdbE5mft'),
+(5, 1, 'Kicking Turbo Granny\'s Ass', '1rU2kuj0NdPMtlXHoNit903Hw5Nbiv3J_'),
+(6, 3, 'Sakura Arrives at Furin', '1rTmSckwKJgPN6gzkTnxaONXjyewYImAT'),
+(7, 3, 'The Hero of My Dreams', '1LM9RJeEm4CIgsuM59GA3KpB8SCHJgsZK'),
+(8, 1, 'Like, Where Are Your Balls?', '1nHeKg_qCj7cBiIfRRtuZJ74WWQKxJul0'),
+(9, 1, 'A Dangerous Woman Arrives', '1VC9XiH6SpzhXOFkmLEfaWIROBUXUAmQh'),
+(10, 1, 'To a Kinder World', '1dBKCbFpPpLCIP8FLjVtpFs8v8VfD5iIe'),
+(11, 1, 'I\'ve Got This Funny Feeling', '15nbbvIs5a21SjEQwiFGC5w3NmdyINtB0'),
+(12, 1, ' Merge! Serpo Dover Demon Nessie!', '17iQluKMENdhLPX_nvPnB4aRRKkOc1L7B'),
+(13, 1, 'Have You Ever Seen a Cattle Mutilation?', '1XWfgC6kwaLGO-wqbuL0PWZre0-M3KTy3'),
+(14, 1, 'First Love', '1Z8OHWKK7lnfS8ZAZ70kCDfLzUKnu5pOz'),
+(15, 1, 'Let\'s Go to the Cursed House', '1qxp8kNM3O_buJEGy6w-25uFG9ZlLbuOZ');
 
 -- --------------------------------------------------------
 
@@ -155,7 +163,7 @@ ALTER TABLE `anime`
 -- AUTO_INCREMENT for table `episodes`
 --
 ALTER TABLE `episodes`
-  MODIFY `episode_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `episode_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `users`
