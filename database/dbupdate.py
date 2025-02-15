3005d837938994d3a44b406543e104d14609a8fb
Bhavin <opbhavin21@gmail.com>
2025-02-16 04:22:48 +0530

    Update database_backup.sql

--- database/dbupdate.py
+++ database/dbupdate.py
@@ -62,4 +62,4 @@ while True:
         import_database()
         file_last_modified_time = current_file_modified_time
 
-    time.sleep(100)  # Check for changes every 100 seconds
+    time.sleep(300)  # Check for changes every 100 seconds
