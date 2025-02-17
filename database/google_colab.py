269c6cc7217a03d40f90f1bcbe5d15e9260e80df
Bhavin <opbhavin21@gmail.com>
2025-02-17 14:07:09 +0530

    Update database_backup.sql

--- database/google_colab.py
+++ database/google_colab.py
@@ -1,14 +1,1 @@
-from google.colab import drive
-import os
 
-# Mount Google Drive
-drive.mount('/content/drive')
-
-# Change this to your anime folder path in Drive
-anime_folder = "/content/drive/My Drive/AnimeFolder"
-
-# List files and extract IDs
-files = os.listdir(anime_folder)
-
-for file in files:
-    print(f"File Name: {file}")
